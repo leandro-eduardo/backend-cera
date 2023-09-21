628dc45511a1c32590bcefc25bde0290c7d1c380
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2023-09-21 03:49:53 -0300

    :recycle: refactor: removes unused error types

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,11 +1,4 @@
-type AppErrorTypes =
-  | 'conflict'
-  | 'not_found'
-  | 'bad_request'
-  | 'unauthorized'
-  | 'not_authenticated'
-  | 'unprocessable_entity'
-  | 'no_data_found';
+type AppErrorTypes = 'conflict' | 'not_found' | 'bad_request' | 'unauthorized';
 
 export interface AppError {
   type: AppErrorTypes;
@@ -19,20 +12,13 @@ export function isAppError(error: object): error is AppError {
 
 export function errorTypeToStatusCode(type: AppErrorTypes) {
   const errorTypes = {
-    unprocessable_entity: 422,
     conflict: 409,
     not_found: 404,
     bad_request: 400,
     unauthorized: 401,
-    not_authenticated: 401,
-    no_data_found: 200,
   };
 
   return errorTypes[type];
-}
-
-export function unprocessableEntityError(message: string): AppError {
-  return { type: 'unprocessable_entity', message };
 }
 
 export function conflictError(message?: string): AppError {
@@ -49,12 +35,4 @@ export function badRequestError(message: string, details?: Object): AppError {
 
 export function unauthorizedError(message: string): AppError {
   return { type: 'unauthorized', message };
-}
-
-export function notAuthenticatedError(message: string): AppError {
-  return { type: 'not_authenticated', message };
-}
-
-export function noDataFound(message: string): AppError {
-  return { type: 'no_data_found', message };
 }
