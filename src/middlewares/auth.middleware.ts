7e6188ecf57389b217e967bbccd703d20595cdbd
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2023-09-21 05:08:28 -0300

    :goal_net: feat: implements jsonwebtoken error handling

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -21,21 +21,14 @@ function getJsonWebTokenSecret() {
 
 export async function auth(req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers;
-  if (!authorization) throw unauthorizedError('missing token');
+  if (!authorization) throw unauthorizedError('missing authorization header');
 
   const tokenParts = authorization.split(' ');
 
-  const [scheme, token] = tokenParts;
+  const [, token] = tokenParts;
 
-  if (!/^Bearer$/i.test(scheme) || tokenParts.length !== 2) throw unauthorizedError('token malformed');
-
-  try {
-    const payload = jwt.verify(token, getJsonWebTokenSecret()) as Payload;
-    const user = await userServices.findById(payload.userId);
-    res.locals.userId = user._id;
-    next();
-  } catch (error: any) {
-    console.log(error);
-    throw unauthorizedError(error.expiredAt ? 'expired token' : 'invalid token');
-  }
+  const payload = jwt.verify(token, getJsonWebTokenSecret()) as Payload;
+  const user = await userServices.findById(payload.userId);
+  res.locals.userId = user._id;
+  next();
 }
--- src/middlewares/errors-handler.middleware.ts
+++ src/middlewares/errors-handler.middleware.ts
@@ -1,7 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { AppError, errorTypeToStatusCode, isAppError } from '@/errors';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 export default function errorsHandler(error: AppError, req: Request, res: Response, next: NextFunction) {
+  if (error instanceof JsonWebTokenError) {
+    return res.status(401).send({
+      type: 'unauthorized',
+      statusCode: 401,
+      message: error.message,
+    });
+  }
+
   if (isAppError(error)) {
     const statusCode = errorTypeToStatusCode(error.type);
     return res
